e7d4e9d7268dba2775f4313a4ddcca14bccb9451
Mateus Devino <mateus@mdevino.com>
2022-04-02 08:40:45 -0300

    TrainingGround page elements update

--- src/main.py
+++ src/main.py
@@ -10,12 +10,31 @@ driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
 page = TrainingGroundPage(driver)
 
 page.go()
+
 print(page.input_field.attribute("name"))
 print(page.input_field.text)
-page.input_field.enter_text("Potato!")
+page.input_field.enter_text("This is the first text field.")
 print(page.input_field.text)
+
 print(page.button1.text)
 print(page.button1.attribute("name"))
-page.button1.click()
+# page.button1.click()
+
+print(page.button2.text)
+print(page.button2.attribute("name"))
+# page.button2.click()
+
+print(page.another_input_field.attribute("name"))
+print(page.another_input_field.text)
+page.another_input_field.enter_text("This is another text field.")
+print(page.another_input_field.text)
+
+print(page.button3.text)
+print(page.button3.attribute("name"))
+# page.button3.click()
+
+print(page.button4.text)
+print(page.button4.attribute("name"))
+# page.button4.click()
 
 driver.quit()
--- src/pages/training_ground.py
+++ src/pages/training_ground.py
@@ -8,9 +8,25 @@ class TrainingGroundPage(BasePage):
     url = "https://techstepacademy.com/training-ground"
 
     @property
+    def input_field(self):
+        return BaseElement(self.driver, "ipt1", By.ID)
+
+    @property
     def button1(self):
         return BaseElement(self.driver, "b1", By.ID)
 
     @property
-    def input_field(self):
-        return BaseElement(self.driver, "ipt1", By.ID)
+    def button2(self):
+        return BaseElement(self.driver, "b1", By.ID)
+
+    @property
+    def another_input_field(self):
+        return BaseElement(self.driver, "ipt2", By.ID)
+
+    @property
+    def button3(self):
+        return BaseElement(self.driver, "b3", By.ID)
+
+    @property
+    def button4(self):
+        return BaseElement(self.driver, "b4", By.ID)
