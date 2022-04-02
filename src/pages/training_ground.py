b8e2bade104fc85b1856703ab0a9b95cc94ab5dd
Mateus Devino <mateus@mdevino.com>
2022-04-01 22:16:49 -0300

    BasePage

--- src/pages/training_ground.py
+++ src/pages/training_ground.py
@@ -1,28 +1,11 @@
 from selenium.webdriver.common.by import By
 
+from .base_page import BasePage
 from ..elements import BaseElement
 
 
-class TrainingGroundPage:
-    def __init__(self, driver):
-        self.url = "https://techstepacademy.com/training-ground"
-        self.driver = driver
-
-    def go(self):
-        self.driver.get(self.url)
-
-    def type_into_input(self, text):
-        input_element = self.driver.find_element_by_id("ipt1")
-        input_element.clear()
-        input_element.send_keys(text)
-
-    def get_input_text(self):
-        input_element = self.driver.find_element_by_id("ipt1")
-        return input_element.get_attribute("value")
-
-    def click_button1(self):
-        button = self.driver.get_element_by_id("b1")
-        button.click()
+class TrainingGroundPage(BasePage):
+    url = "https://techstepacademy.com/training-ground"
 
     @property
     def button1(self):
